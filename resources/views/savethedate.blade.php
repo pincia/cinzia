66f82a1a1304e642058c04bcb28b0403599d2961
federico <f.pinciaroli@italprogetti.it>
2019-10-15 18:16:40 +0200

    PRIMO COMMIT

---
+++ resources/views/savethedate.blade.php
@@ -0,0 +1,397 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html>
+<head>
+    <meta charset="UTF-8">
+   
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    <link rel="stylesheet" href={{ asset ("bower_components/bootstrap/dist/css/bootstrap.min.css") }}>
+
+    <link rel="stylesheet" href={{ asset ("css/styles.css") }}>
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href={{ asset ("bower_components/font-awesome/css/font-awesome.min.css") }}>
+  <!-- Ionicons -->
+  <link rel="stylesheet" href={{ asset ("bower_components/Ionicons/css/ionicons.min.css") }}>
+  <!-- Theme style -->
+  <link rel="stylesheet" href={{ asset ("bower_components/admin-lte/dist/css/AdminLTE.min.css") }}>
+  <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
+        page. However, you can choose any other skin. Make sure you
+        apply the skin class to the body tag so the changes take effect. -->
+  <link rel="stylesheet" href={{ asset ("bower_components/admin-lte/dist/css/skins/skin-black-light.min.css") }}>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body style="background:black;">
+
+
+    <!-- Header 
+
+-->
+    <!-- Sidebar -->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div style="background: black;margin: 20px;">
+             <!-- Main content -->
+
+
+            <svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width=100% height=100% viewBox="0 0 595.28 841.89" enable-background="new 0 0 595.28 841.89" xml:space="preserve">
+<rect x="2.5" width="592.781" height="841.89"/>
+<g>
+	<defs>
+		<rect id="SVGID_55_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_1_">
+		<use xlink:href="#SVGID_55_"  overflow="visible"/>
+	</clipPath>
+	<rect x="2.5" clip-path="url(#SVGID_1_)" fill="none" stroke="#FFFFFF" stroke-miterlimit="10" width="595.275" height="841.89"/>
+</g>
+<path fill="#E30613" d="M32,433.676V8.568V433.676z"/>
+<path fill="#D48C39" d="M32.998,433.676L33,3.569L32.998,433.676z"/>
+<g>
+	<defs>
+		<rect id="SVGID_57_" x="1" width="594.281" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_2_">
+		<use xlink:href="#SVGID_57_"  overflow="visible"/>
+	</clipPath>
+	
+		<line clip-path="url(#SVGID_2_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" x1="32.999" y1="3.569" x2="32.998" y2="433.676"/>
+</g>
+<g>
+	<defs>
+		<rect id="SVGID_59_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_3_">
+		<use xlink:href="#SVGID_59_"  overflow="visible"/>
+	</clipPath>
+	
+		<rect x="1" clip-path="url(#SVGID_3_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" width="595.276" height="841.891"/>
+</g>
+<path fill="#D48C39" d="M121.151,329.952V2.994V329.952z"/>
+<g>
+	<defs>
+		<rect id="SVGID_61_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_4_">
+		<use xlink:href="#SVGID_61_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_4_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M121.15,329.952V2.994
+		V329.952z"/>
+</g>
+<text transform="matrix(1 0 0 1 222.6011 481.5688)" fill="#D48C39" font-family="'HazelDecoLight-Regular'" font-size="36"> </text>
+<line fill="none" stroke="#E30613" stroke-width="5" stroke-miterlimit="10" x1="177.065" y1="764.629" x2="572" y2="763.629"/>
+<path fill="#D48C39" d="M212.298,302.951V2.994V302.951z"/>
+<g>
+	<defs>
+		<rect id="SVGID_63_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_5_">
+		<use xlink:href="#SVGID_63_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_5_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M212.298,302.952V2.994
+		V302.952z"/>
+</g>
+<path fill="#D48C39" d="M279,420.946V2V420.946z"/>
+<g>
+	<defs>
+		<rect id="SVGID_65_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_6_">
+		<use xlink:href="#SVGID_65_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_6_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M279,420.946V2.001
+		V420.946z"/>
+</g>
+<path fill="#D48C39" d="M276,420.946h315.28H276z"/>
+<g>
+	<defs>
+		<rect id="SVGID_67_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_7_">
+		<use xlink:href="#SVGID_67_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_7_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M591.28,420.945H276
+		H591.28z"/>
+</g>
+<path fill="#D48C39" d="M306,392.154V2V392.154z"/>
+<g>
+	<defs>
+		<rect id="SVGID_69_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_8_">
+		<use xlink:href="#SVGID_69_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_8_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M306,392.154V2.001
+		V392.154z"/>
+</g>
+<path fill="#D48C39" d="M303,392.154h286.561H303z"/>
+<g>
+	<defs>
+		<rect id="SVGID_71_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_9_">
+		<use xlink:href="#SVGID_71_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_9_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M589.561,392.154
+		H302.999H589.561z"/>
+</g>
+<path fill="#D48C39" d="M303,91.719h292.28H303z"/>
+<g>
+	<defs>
+		<rect id="SVGID_73_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_10_">
+		<use xlink:href="#SVGID_73_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_10_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M595.28,91.718H303
+		H595.28z"/>
+</g>
+<path fill="#D48C39" d="M303,179.038h292.28H303z"/>
+<g>
+	<defs>
+		<rect id="SVGID_75_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_11_">
+		<use xlink:href="#SVGID_75_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_11_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="303,179.037 
+		595.28,179.037 303,179.037 	"/>
+</g>
+<path fill="#D48C39" d="M303,269.504h292.28H303z"/>
+<g>
+	<defs>
+		<rect id="SVGID_77_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_12_">
+		<use xlink:href="#SVGID_77_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_12_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="303,269.505 
+		595.28,269.505 303,269.505 	"/>
+</g>
+<path fill="#D48C39" d="M361.458,443.792h233.823H361.458z"/>
+<g>
+	<defs>
+		<rect id="SVGID_79_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_13_">
+		<use xlink:href="#SVGID_79_"  overflow="visible"/>
+	</clipPath>
+	
+		<line clip-path="url(#SVGID_13_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" x1="595.28" y1="443.792" x2="361.458" y2="443.792"/>
+</g>
+<path fill="#D48C39" d="M363.458,462.569h231.823H363.458z"/>
+<g>
+	<defs>
+		<rect id="SVGID_81_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_14_">
+		<use xlink:href="#SVGID_81_"  overflow="visible"/>
+	</clipPath>
+	
+		<line clip-path="url(#SVGID_14_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" x1="597.28" y1="462.569" x2="363.458" y2="462.569"/>
+</g>
+<path fill="#D48C39" d="M363.458,481.569h231.823H363.458z"/>
+<g>
+	<defs>
+		<rect id="SVGID_83_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_15_">
+		<use xlink:href="#SVGID_83_"  overflow="visible"/>
+	</clipPath>
+	
+		<line clip-path="url(#SVGID_15_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" x1="597.28" y1="481.569" x2="363.458" y2="481.569"/>
+</g>
+<path fill="#D48C39" d="M0,436.829h48.431H0z"/>
+<g>
+	<defs>
+		<rect id="SVGID_85_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_16_">
+		<use xlink:href="#SVGID_85_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_16_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="0,436.829 
+		48.431,436.829 0,436.829 	"/>
+</g>
+<path fill="#D48C39" d="M0,458.266h48.431H0z"/>
+<g>
+	<defs>
+		<rect id="SVGID_87_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_17_">
+		<use xlink:href="#SVGID_87_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_17_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="0,458.266 
+		48.431,458.266 0,458.266 	"/>
+</g>
+<path fill="#D48C39" d="M0,476.622h46.287H0z"/>
+<g>
+	<defs>
+		<rect id="SVGID_89_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_18_">
+		<use xlink:href="#SVGID_89_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_18_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="
+		-2.144,476.622 46.288,476.622 -2.144,476.622 	"/>
+</g>
+<path fill="#D48C39" d="M0,502.5h154.025H0z"/>
+<g>
+	<defs>
+		<rect id="SVGID_91_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_19_">
+		<use xlink:href="#SVGID_91_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_19_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="
+		-1.671,502.5 154.025,502.5 -1.671,502.5 	"/>
+</g>
+<path fill="#D48C39" d="M1,528.5h125.501H1z"/>
+<g>
+	<defs>
+		<rect id="SVGID_93_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_20_">
+		<use xlink:href="#SVGID_93_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_20_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="1,528.5 
+		126.5,528.5 1,528.5 	"/>
+</g>
+<polygon fill="#D48C39" points="2.475,552 0,552 105.975,552 "/>
+<g>
+	<defs>
+		<rect id="SVGID_95_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_21_">
+		<use xlink:href="#SVGID_95_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_21_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="2.475,552 
+		105.975,552 -0.525,552 	"/>
+</g>
+<path fill="#D48C39" d="M103,839.896V552V839.896z"/>
+<g>
+	<defs>
+		<rect id="SVGID_97_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_22_">
+		<use xlink:href="#SVGID_97_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_22_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M103,839.896V552
+		V839.896z"/>
+</g>
+<path fill="#D48C39" d="M125.5,841.89l-2-315.895L125.5,841.89z"/>
+<g>
+	<defs>
+		<rect id="SVGID_99_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_23_">
+		<use xlink:href="#SVGID_99_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_23_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M125.5,841.89
+		l-2-315.896L125.5,841.89z"/>
+</g>
+<path fill="#D48C39" d="M154.025,841.89V499.719V841.89z"/>
+<g>
+	<defs>
+		<rect id="SVGID_101_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_24_">
+		<use xlink:href="#SVGID_101_"  overflow="visible"/>
+	</clipPath>
+	<path clip-path="url(#SVGID_24_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" d="M154.025,841.89
+		V499.719V841.89z"/>
+</g>
+<path fill="#D48C39" d="M1,650.667h102.5H1z"/>
+<g>
+	<defs>
+		<rect id="SVGID_103_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_25_">
+		<use xlink:href="#SVGID_103_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_25_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="1,650.667 
+		103.5,650.667 1,650.667 	"/>
+</g>
+<polygon fill="#D48C39" points="0,739.999 1,739.999 103.5,739.999 "/>
+<g>
+	<defs>
+		<rect id="SVGID_105_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_26_">
+		<use xlink:href="#SVGID_105_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_26_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="0,739.999 
+		103.5,739.999 1,739.999 	"/>
+</g>
+<polygon fill="#D48C39" points="0,827.999 2.5,827.999 102.5,827.999 "/>
+<g>
+	<defs>
+		<rect id="SVGID_107_" width="595.28" height="841.89"/>
+	</defs>
+	<clipPath id="SVGID_27_">
+		<use xlink:href="#SVGID_107_"  overflow="visible"/>
+	</clipPath>
+	<polyline clip-path="url(#SVGID_27_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="-1,827.999 
+		102.5,827.999 2.5,827.999 	"/>
+	
+		<circle clip-path="url(#SVGID_27_)" fill="none" stroke="#D48C39" stroke-width="10" stroke-miterlimit="10" cx="205.607" cy="461.077" r="158.857"/>
+	<path clip-path="url(#SVGID_27_)" fill="#D48C39" d="M227.919,692h292.579H227.919z"/>
+	<polyline clip-path="url(#SVGID_27_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="227.919,692 
+		520.498,692 227.919,692 	"/>
+	<path clip-path="url(#SVGID_27_)" fill="#D48C39" d="M177.065,764.629H572H177.065z"/>
+	<polyline clip-path="url(#SVGID_27_)" fill="none" stroke="#D48C39" stroke-width="6" stroke-miterlimit="10" points="
+		177.065,764.629 572,764.629 177.065,764.629 	"/>
+</g>
+<text transform="matrix(1 0 0 1 227.9194 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">S</text>
+<text transform="matrix(1 0 0 1 252.2197 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">A</text>
+<text transform="matrix(1 0 0 1 279.3271 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">VE T</text>
+<text transform="matrix(1 0 0 1 366.7354 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">H</text>
+<text transform="matrix(1 0 0 1 391.7197 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">E </text>
+<text transform="matrix(1 0 0 1 428.5479 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">D</text>
+<text transform="matrix(1 0 0 1 453.7471 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">A</text>
+<text transform="matrix(1 0 0 1 481.7197 670.6968)" fill="#D48C39" font-family="Raconteur NF" font-size="36">TE</text>
+<text transform="matrix(1 0 0 1 73.7026 450.1616)" fill="#D48C39" font-family="Raconteur NF" font-size="51">CINZIA </text>
+<text transform="matrix(1 0 0 1 284.3521 547.1216)" fill="#D48C39" font-family="Raconteur NF" font-size="24">th  </text>
+<text transform="matrix(1 0 0 1 177.0649 547.1216)" fill="#D48C39" font-family="Raconteur NF"font-size="78">30</text>
+<text transform="matrix(1 0 0 1 297.6396 740.3745)" fill="#D48C39" font-family="Raconteur NF"font-size="36">25.11.2019</text>
+<text transform="matrix(1 0 0 1 177.0762 815.687)" fill="#D48C39" font-family="Raconteur NF" font-size="36">prohibition party</text>
+</svg>
+
+        <div style=" width:100%;text-align:center;">
+<img src = "/images/key_oro.svg" style="width:30%;">
+        <form method="post" action = "/create">
+        @csrf <!-- This blade directive generates <input type="hidden" name="_token" value="xyz..." /> -->
+        <input type="text" name="code" id="fname" placeholder="Inserisci il tuo CODICE" /><br/><br/>
+        <input class="btn btn-warning btn-lg" type="submit" name="submit" id="submit" />
+        <div style="height: 20px;"></div>
+</form>
+</div>
+    </div><!-- /.content-wrapper -->
+
+    <!-- Footer -->
+
+
+<!-- REQUIRED JS SCRIPTS -->
+
+<!-- jQuery 2.1.3 -->
+<script src={{ asset ("bower_components/jquery/dist/jquery.min.js") }}></script>
+<!-- Bootstrap 3.3.7 -->
+<script src={{ asset ("bower_components/bootstrap/dist/js/bootstrap.min.js") }}></script>
+<!-- AdminLTE App -->
+<script src={{ asset ("bower_components/admin-lte/dist/js/adminlte.min.js") }}></script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+      Both of these plugins are recommended to enhance the
+      user experience -->
+</body>
+</html>
