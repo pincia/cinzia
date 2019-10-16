58c9678ea4f6bcdf8ca5fd3d029188c22776d951
federico <f.pinciaroli@italprogetti.it>
2019-10-16 02:29:15 +0200

    test

--- resources/views/dresscode.blade.php
+++ resources/views/dresscode.blade.php
@@ -8,6 +8,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <meta charset="UTF-8">
    
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.css">
+<link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+
+<script src="https://unpkg.com/swiper/js/swiper.js"></script>
+<script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
     <!-- Bootstrap 3.3.2 -->
     <link rel="stylesheet" href={{ asset ("bower_components/bootstrap/dist/css/bootstrap.min.css") }}>
   <!-- Font Awesome -->
@@ -29,6 +34,48 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
+    <style>
+    html, body {
+      position: relative;
+      height: 100%;
+    }
+    body {
+      background: #eee;
+      font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+      font-size: 14px;
+      color:#000;
+      margin: 0;
+      padding: 0;
+    }
+    .swiper-container {
+      width: 100%;
+    
+    }
+    .swiper-button-next{
+          color: #D48C39;
+    }
+    .swiper-button-prev{
+          color: #D48C39;
+    }
+    .swiper-slide {
+      text-align: center;
+      font-size: 18px;
+      background: #fff;
+      /* Center slide text vertically */
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: -webkit-flex;
+      display: flex;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      -webkit-justify-content: center;
+      justify-content: center;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      -webkit-align-items: center;
+      align-items: center;
+    }
+  </style>
 </head>
 <body class="skin-black" style="background:black!important">
 
@@ -40,8 +87,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Content Wrapper. Contains page content -->
     <div style="background: black!important;margin: 20px;">
 
-		
-</div>
+    <div class="swiper-container">
+    <div class="swiper-wrapper">
+    <div class="swiper-slide" style="background-image:url(./images/foto1.jpg)"></div>
+      <div class="swiper-slide" style="background-image:url(./images/foto2.jpg)"></div>
+      <div class="swiper-slide" style="background-image:url(./images/foto3.jpg)"></div>
+      <div class="swiper-slide" style="background-image:url(./images/foto4.jpg)"></div>
+      <div class="swiper-slide" style="background-image:url(./images/foto5.jpg)"></div>
+    </div>
+    <!-- Add Arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+  </div>
+      </div>
 
     
 
@@ -57,5 +115,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <!-- Optionally, you can add Slimscroll and FastClick plugins.
       Both of these plugins are recommended to enhance the
       user experience -->
+      <script>
+    var swiper = new Swiper('.swiper-container', {
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  </script>
 </body>
+
 </html>
